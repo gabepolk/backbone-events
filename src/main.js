dc4c597be2e63b259f84175ac00df78e3067c445
Gabe Polk <gabe.a.polk@gmail.com>
2014-07-10 13:39:53 -0700

    Added profile view + underscore.js template

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
-var ShowButtonView = Backbone.View.extend({
-  render: function() {
+var ButtonView = Backbone.View.extend({
+  render: function(){
     $(this.el).html('<button>I do nothing!</button>');
   }
 });
 
-var buttonView = new ShowButtonView({
+var buttonView = new ButtonView({
   el: $('.my-button')
 });
 
 buttonView.render();
+
+var ProfileView = Backbone.View.extend({
+  initialize: function(options){
+    this.name = options.name;
+    this.age = options.age;
+    var profileTemplateHtml = $('#templates .profile-template').html();
+    this.template = _.template(profileTemplateHtml);
+  },
+  render: function(){
+    $(this.el).empty();
+    var newHtml = this.template({ name: this.name, age: this.age });
+    $(this.el).append(newHtml);
+  }
+});
+
+var ProfileView = new ProfileView({
+  name: 'Bob',
+  age: 45,
+  el: $('.profile')
+});
+
+ProfileView.render();
